4f042f3802255ac964342048a04e72f820b7687c
Maksim Sisov <msisov@igalia.com>
2017-10-17 15:13:32 +0300

    [ozone/mus] Add inital support for a tab drag window.
    This patch adds an initial support to start a move loop and create
    a new window by dragging a tab. To start with, this commit
    extends WindowTree::PerformWindowMove, which calls
    WindowServer::StartMoveLoop -> PlatformWindow::RunMoveLoop.
    
    X11 Ozone implementation of PlatformWindow uses
    WindowMoveLoopClient, which instantiates the move loop and
    WholeScreenMoveLoop, which creates an invisible window
    and intercepts all the events from it. Then the system screen
    location is taken and sent to WindowMoveLoopClient, which
    updates actual bounds of X11WindowBase.
    
    The way it works is precisly the same as in stock X11, but
    further work to share the code is needed, because there are
    some difference between stock x11 and ozone x11 event
    handlings, bounds set and etc.
    
    Issue #264
    
    fixup! [ozone/mus] Add inital support for a tab drag window.
    
    adapt to
    https://crrev.com/c/774778
    
    CanDispatchEvent is modified in such a way that it also tests if
    the client in a move loop right now. If so, it must dispatch events
    further once whole_screen_move_loop processes them in order to update
    mouse locations on aura side.

--- ui/ozone/platform/drm/host/drm_window_host.cc
+++ ui/ozone/platform/drm/host/drm_window_host.cc
@@ -135,6 +135,12 @@ PlatformImeController* DrmWindowHost::GetPlatformImeController() {
 
 void DrmWindowHost::PerformNativeWindowDragOrResize(uint32_t hittest) {}
 
+bool DrmWindowHost::RunMoveLoop(const gfx::Vector2d& drag_offset) {
+  return false;
+}
+
+void DrmWindowHost::StopMoveLoop() {}
+
 bool DrmWindowHost::CanDispatchEvent(const PlatformEvent& event) {
   DCHECK(event);
 
