08eb18560df6cabee00f265444b98fe99ce3fa41
Maksim Sisov <msisov@igalia.com>
2017-10-17 15:13:32 +0300

    Add inital support for a tab drag window.
    This patch adds an initial support to start a move loop and create
    a new window by dragging a tab. To start with, this commit
    extends WindowTree::PerformWindowMove, which calls
    WindowServer::StartMoveLoop -> PlatformWindow::RunMoveLoop.
    
    X11 Ozone implementation of PlatformWindow uses
    WindowMoveLoopClient, which instantiates the move loop and
    WholeScreenMoveLoop, which creates an invisible window
    and intercepts all the events from it. Then the system screen
    location is taken and sent to WindowMoveLoopClient, which
    updates actual bounds of X11WindowBase.
    
    The way it works is precisly the same as in stock X11, but
    further work to share the code is needed, because there are
    some difference between stock x11 and ozone x11 event
    handlings, bounds set and etc.
    
    Issue #264
    
    fixup! [ozone/wayland] Add inital support for a tab drag window.
    
    adapt to
    https://crrev.com/c/774778
    
    CanDispatchEvent is modified in such a way that it also tests if
    the client in a move loop right now. If so, it must dispatch events
    further once whole_screen_move_loop processes them in order to update
    mouse locations on aura side.
    
    fixup! [ozone/wayland] Add inital support for a tab drag window.
    
    It removed changes at services from previous change to make it
    available without mus and made WindowFinder with ozone work without
    mus with creating GetLocalProcessWindowAtPointOzone to
    get gfx::NativeWindow at input position.
    
    Issue #430
    
    fixup! Add inital support for a tab drag window.
    
    Remove the non-existing code and fix ozone/drm build.
    
    Change-Id: I9d3a94a9e47ed2bddaef0253de193d92e01768b0

--- ui/ozone/platform/wayland/wayland_window.h
+++ ui/ozone/platform/wayland/wayland_window.h
@@ -120,6 +120,8 @@ class WaylandWindow : public PlatformWindow,
   PlatformImeController* GetPlatformImeController() override;
   void SetRestoredBoundsInPixels(const gfx::Rect& bounds) override;
   gfx::Rect GetRestoredBoundsInPixels() const override;
+  bool RunMoveLoop(const gfx::Vector2d& drag_offset) override;
+  void StopMoveLoop() override;
 
   // PlatformEventDispatcher
   bool CanDispatchEvent(const PlatformEvent& event) override;
