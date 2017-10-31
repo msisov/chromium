ec581e3ff0914e75789021022ca6397f4e26bc72
Antonio Gomes <tonikitoo@igalia.com>
2017-10-31 15:52:34 -0400

    [ozone/wayland] Implement Tooltip using subsurface
    CL uses the subsurface concept to implement support for tooltips.
    
    Issue #86
    
    fixup! [ozone/wayland] Implement Tooltip using subsurface
    
    Fix crash on gnome shell by destroying a subsurface only after a
    buffer is detached.
    
    fixup! [ozone/wayland] Implement Tooltip using subsurface
    
    This CL takes care of two issues:
    
    1) By the time one hovers mouse over something that creates a tooltip,
    a message is sent from the client (browser/aura) to the server (mus).
    It is async call, so when mus actually gets the message and starts
    the logic that creates/shows a tooltip window, user might have already
    moved the mouse off the window bound.
    When pointer moves off-window, WaylandWindow::GetParentWindow will return NULL.
    Patch turns the existing DCHECK into an early return check.
    
    2) When one moves the mouse off the chrome window, emit a ET_MOUSE_EXITED
    mouse event, so that ToolTipController has (see view::corewm::GetTooltipTarget).
    
    Issue #295
    
    fixup! [ozone/wayland] Implement Tooltip using subsurface
    
    We are now emitting ET_MOUSE_EXITED upon pointer leaving WL events.
    CL updates the associated unittest's expectation: WaylandPointerTest::Leave.
    
    Issue #295

--- ui/ozone/platform/wayland/wayland_window.h
+++ ui/ozone/platform/wayland/wayland_window.h
@@ -122,6 +122,8 @@ class WaylandWindow : public PlatformWindow, public PlatformEventDispatcher {
   void CreateXdgPopup();
   // Creates a surface window, which is visible as a main window.
   void CreateXdgSurface();
+  // Creates a subsurface window, to host tooltip's content.
+  void CreateTooltipSubSurface();
 
   // Gets a parent window for this window.
   WaylandWindow* GetParentWindow();
@@ -135,6 +137,7 @@ class WaylandWindow : public PlatformWindow, public PlatformEventDispatcher {
   std::unique_ptr<XDGShellObjectFactory> xdg_shell_objects_factory_;
 
   wl::Object<wl_surface> surface_;
+  wl::Object<wl_subsurface> tooltip_subsurface_;
 
   // Wrappers around xdg v5 and xdg v6 objects. WaylandWindow doesn't
   // know anything about the version.
@@ -157,6 +160,8 @@ class WaylandWindow : public PlatformWindow, public PlatformEventDispatcher {
   ui::PlatformWindowState state_;
 
   bool is_active_ = false;
+
+  bool is_tooltip_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(WaylandWindow);
 };
